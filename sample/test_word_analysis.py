8f40d13636cb2e85ecc97120ae6acf0723f8bf3d
collinspg <collinsokolie90@gmail.com>
2023-12-11 12:18:21 +0100

    my project work

--- sample/test_word_analysis.py
+++ sample/test_word_analysis.py
@@ -2,17 +2,32 @@ import pytest
 from word_analysis import calculate_word_frequency, calculate_word_count
 
 def test_calculate_word_count():
-    text = "Once upon a time, in a faraway land, there lived a young princess named Aurora."
-    word_count = calculate_word_count(text)
-    assert word_count == 15
+    text1 = "Once upon a time, in a faraway land, there lived a young princess named Aurora."
+    text2 = "She had flowing golden hair, sparkling blue eyes, and a kind heart."
+    
+    word_count1 = calculate_word_count(text1)
+    assert word_count1 == 15
+    
+    word_count2 = calculate_word_count(text2)
+    assert word_count2 == 12
 
 def test_calculate_word_frequency():
-    text = "Once upon a time, in a faraway land, there lived a young princess named Aurora."
-    word_frequency = calculate_word_frequency(text)
-    expected_frequency = {
+    text1 = "Once upon a time, in a faraway land, there lived a young princess named Aurora."
+    text2 = "She had flowing golden hair, sparkling blue eyes, and a kind heart."
+    
+    word_frequency1 = calculate_word_frequency(text1)
+    expected_frequency1 = {
         'Once': 1, 'upon': 1, 'a': 3, 'time': 1, 'in': 1, 'faraway': 1, 'land': 1,
         'there': 1, 'lived': 1, 'young': 1, 'princess': 1, 'named': 1, 'Aurora': 1
     }
-    assert word_frequency == expected_frequency
-
-pytest.main(["-v", "--tb=line", "-rN", __file__])  
+    
+    assert word_frequency1 == expected_frequency1
+    
+    word_frequency2 = calculate_word_frequency(text2)
+    expected_frequency2 = {
+        'She': 1, 'had': 1, 'flowing': 1, 'golden': 1, 'hair': 1, 'sparkling': 1,
+        'blue': 1, 'eyes': 1, 'and': 1, 'a': 1, 'kind': 1, 'heart': 1
+    }
+    
+    assert word_frequency2 == expected_frequency2
+pytest.main(["-v", "--tb=line", "-rN", __file__])
--- sample/word_analysis.py
+++ sample/word_analysis.py
@@ -10,6 +10,7 @@ def main():
         return
     except PermissionError:
         print(f"Error: Permission denied. Please check file permissions.")
+        return
         
     total_word_count = calculate_word_count(text)
     print(f'Total Word Count: {total_word_count}')
@@ -21,8 +22,7 @@ def main():
     print('Word Frequency:')
     for word, frequency in word_frequency.items():
         print(f'{word}: {frequency}')
-        
-        
+
 def read_file(filename):
     try:
         with open(filename, 'rt') as file:
@@ -31,27 +31,42 @@ def read_file(filename):
         raise FileNotFoundError(f"File '{filename}' not found.")
     except PermissionError:
         raise PermissionError(f"Permission denied for file '{filename}'.")
-    
+
 def calculate_word_count(text):
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    words = text.split()
+    # Remove punctuation from the text
+    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Split the text into words
+    words = cleaned_text.split()
+
+    # Return the count of words
     return len(words)
+
 
 def calculate_unique_word_count(text):
     words = text.split()
     unique_words = set(words)
     return len(unique_words)
 
+
 def calculate_word_frequency(text):
     word_frequency = {}
-    words = text.split()
+    # Remove punctuation from the text
+    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Split the text into words
+    words = cleaned_text.split()
+
+    # Calculate word frequency
     for word in words:
-        word = word.strip(",.")
         if word in word_frequency:
             word_frequency[word] += 1
         else:
             word_frequency[word] = 1
+
     return word_frequency
+
+
 
 
 
